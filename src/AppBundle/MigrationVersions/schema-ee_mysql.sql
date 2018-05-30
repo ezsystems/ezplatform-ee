92cf1b46c995f0f05d00c425bac93db33a68727f
Damian Zabawa <damian.zabawa@ez.no>
2018-05-30 11:29:22 +0200

    Added missing dbp schema, `composer install-ee-demo` command

--- src/AppBundle/MigrationVersions/schema-ee_mysql.sql
+++ src/AppBundle/MigrationVersions/schema-ee_mysql.sql
@@ -1,3 +1,37 @@
+DROP TABLE IF EXISTS `ezflexworkflow`;
+CREATE TABLE `ezflexworkflow` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `message_id` int(11) DEFAULT NULL,
+  `content_id` int(11) NOT NULL,
+  `content_owner_id` int(11) NOT NULL,
+  `previous_owner_id` int(11) NOT NULL,
+  `new_owner_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `created` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `message_id` (`message_id`),
+  KEY `content_id` (`content_id`),
+  KEY `content_owner_id` (`content_owner_id`),
+  KEY `previous_owner_id` (`previous_owner_id`),
+  KEY `new_owner_id` (`new_owner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ezflexworkflow_message`;
+CREATE TABLE `ezflexworkflow_message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `content_id` int(11) NOT NULL,
+  `message` blob DEFAULT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `created` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `content_id` (`content_id`),
+  KEY `sender_id` (`sender_id`),
+  KEY `receiver_id` (`receiver_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `eznotification`;
 CREATE TABLE `eznotification` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -9,6 +43,21 @@ CREATE TABLE `eznotification` (
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   KEY `is_pending` (`is_pending`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ezdatebasedpublisher_scheduled_version`;
+CREATE TABLE `ezdatebasedpublisher_scheduled_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content_id` int(11) NOT NULL,
+  `version_id` int(11) NOT NULL,
+  `version_number` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `publication_date` int(11) NOT NULL,
+  `url_root` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index4` (`content_id`,`version_number`),
+  KEY `index2` (`content_id`),
+  KEY `index3` (`version_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
