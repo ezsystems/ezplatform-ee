1ff2471ba75c134d4c21e2d4369094a646410868
Maciej Kobus <maciej.kobus@ez.no>
2019-06-17 12:12:47 +0200

    Added missing configuration for EE packages

--- config/bundles.php
+++ config/bundles.php
@@ -23,6 +23,8 @@ return [
     Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     // eZ Platform
     eZ\Bundle\EzPublishCoreBundle\EzPublishCoreBundle::class => ['all' => true],
     eZ\Bundle\EzPublishLegacySearchEngineBundle\EzPublishLegacySearchEngineBundle::class => ['all' => true],
@@ -44,5 +46,13 @@ return [
     EzSystems\EzPlatformAdminUiAssetsBundle\EzPlatformAdminUiAssetsBundle::class => ['all' => true],
     EzSystems\EzPlatformEncoreBundle\EzSystemsEzPlatformEncoreBundle::class => ['all' => true],
     EzSystems\EzPlatformMatrixFieldtypeBundle\EzPlatformMatrixFieldtypeBundle::class => ['all' => true],
-    EzSystems\BehatBundle\EzSystemsBehatBundle::class => ['behat' => true]
+    EzSystems\BehatBundle\EzSystemsBehatBundle::class => ['behat' => true],
+    // eZ Platform EE
+    EzSystems\EzPlatformPageBuilderBundle\EzPlatformPageBuilderBundle::class => ['all' => true],
+    EzSystems\EzPlatformPageFieldTypeBundle\EzPlatformPageFieldTypeBundle::class => ['all' => true],
+    EzSystems\EzPlatformFormBuilderBundle\EzPlatformFormBuilderBundle::class => ['all' => true],
+    EzSystems\DateBasedPublisherBundle\EzSystemsDateBasedPublisherBundle::class => ['all' => true],
+    EzSystems\FlexWorkflowBundle\EzSystemsFlexWorkflowBundle::class => ['all' => true],
+    EzSystems\EzPlatformEnterpriseEditionInstallerBundle\EzPlatformEnterpriseEditionInstallerBundle::class => ['all' => true],
+    EzSystems\EzPlatformWorkflowBundle\EzPlatformWorkflowBundle::class => ['all' => true],
 ];
