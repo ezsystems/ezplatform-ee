99c69a69a6cac6582fe45a6367c8a29da2605fb8
Dawid Parafiński <dawid.parafinski@ez.no>
2020-03-16 10:23:58 +0100

    EZEE-2995: Renamed ContentComparison Bundle to Version Comparison (#126)

--- config/bundles.php
+++ config/bundles.php
@@ -61,7 +61,7 @@ return [
     EzSystems\EzPlatformCalendarBundle\EzPlatformCalendarBundle::class => ['all' => true],
     EzSystems\EzPlatformGraphQL\EzSystemsEzPlatformGraphQLBundle::class => ['all' => true],
     EzSystems\EzPlatformQueryFieldType\Symfony\EzSystemsEzPlatformQueryFieldTypeBundle::class => ['all' => true],
-    EzSystems\EzPlatformContentComparisonBundle\EzPlatformContentComparisonBundle::class => ['all' => true],
+    EzSystems\EzPlatformVersionComparisonBundle\EzPlatformVersionComparisonBundle::class => ['all' => true],
     // GraphQL bundles have to be loaded after EzSystemsEzPlatformGraphQLBundle
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
