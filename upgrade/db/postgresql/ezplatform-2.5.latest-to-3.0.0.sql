7e8f73985cd01cf0cb56dba8ec1b184a8a1ddef2
Andrew Longosz <alongosz@users.noreply.github.com>
2020-07-20 12:37:20 +0200

    EZEE-3214: Dropped default value from Workflow Markings message column (#149)

--- upgrade/db/postgresql/ezplatform-2.5.latest-to-3.0.0.sql
+++ upgrade/db/postgresql/ezplatform-2.5.latest-to-3.0.0.sql
@@ -37,7 +37,7 @@ UPDATE "ezcontentclass_attribute" SET "data_text2" = '^[^@]+$'
 
 -- EZEE-2880: Added support for stage and transition actions --
 ALTER TABLE ezeditorialworkflow_markings
-    ADD COLUMN message TEXT NOT NULL default '',
+    ADD COLUMN message TEXT NOT NULL,
     ADD COLUMN reviewer_id INTEGER,
     ADD COLUMN result TEXT;
 --
