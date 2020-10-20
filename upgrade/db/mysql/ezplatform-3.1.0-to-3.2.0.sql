2fa55ac084d31653621f329503e2ed8cf738a985
ciastektk <tomasz.kryszan@ez.no>
2020-10-20 17:22:41 +0200

    Added SQL migrations (#165)

--- upgrade/db/mysql/ezplatform-3.1.0-to-3.2.0.sql
+++ upgrade/db/mysql/ezplatform-3.1.0-to-3.2.0.sql
@@ -1,3 +1,35 @@
 -- EZEE-3244: Added path to site configuration --
 ALTER TABLE `ezsite_public_access` ADD COLUMN site_matcher_path VARCHAR(255) DEFAULT NULL;
 --
+
+DROP TABLE IF EXISTS `ibexa_segment_group_map`;
+CREATE TABLE `ibexa_segment_group_map` (
+  `segment_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`segment_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+DROP TABLE IF EXISTS `ibexa_segment_groups`;
+CREATE TABLE `ibexa_segment_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`identifier`),
+  UNIQUE KEY `ibexa_segment_groups_identifier` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+DROP TABLE IF EXISTS `ibexa_segment_user_map`;
+CREATE TABLE `ibexa_segment_user_map` (
+  `segment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`segment_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+DROP TABLE IF EXISTS `ibexa_segments`;
+CREATE TABLE `ibexa_segments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`identifier`),
+  UNIQUE KEY `ibexa_segments_identifier` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
--- upgrade/db/postgresql/ezplatform-3.1.0-to-3.2.0.sql
+++ upgrade/db/postgresql/ezplatform-3.1.0-to-3.2.0.sql
@@ -1,3 +1,35 @@
 -- EZEE-3244: Added path to site configuration --
 ALTER TABLE `ezsite_public_access` ADD COLUMN site_matcher_path VARCHAR(255) DEFAULT NULL;
 --
+
+DROP TABLE IF EXISTS ibexa_segment_group_map;
+CREATE TABLE ibexa_segment_group_map (
+  segment_id int NOT NULL,
+  group_id int NOT NULL,
+  PRIMARY KEY (segment_id,group_id)
+);
+
+DROP TABLE IF EXISTS ibexa_segment_groups;
+CREATE TABLE ibexa_segment_groups (
+  id int NOT NULL,
+  identifier varchar(255) NOT NULL,
+  name varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (id,identifier),
+  CONSTRAINT ibexa_segment_groups_identifier UNIQUE (identifier)
+);
+
+DROP TABLE IF EXISTS ibexa_segment_user_map;
+CREATE TABLE ibexa_segment_user_map (
+  segment_id int NOT NULL,
+  user_id int NOT NULL,
+  PRIMARY KEY (segment_id,user_id)
+);
+
+DROP TABLE IF EXISTS ibexa_segments;
+CREATE TABLE ibexa_segments (
+  id int NOT NULL,
+  identifier varchar(255) NOT NULL,
+  name varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (id,identifier),
+  CONSTRAINT ibexa_segments_identifier UNIQUE (identifier)
+);
