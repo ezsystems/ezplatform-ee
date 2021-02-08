db88360e53af2f8f5a70aaee757e1d4e1588eb92
mateuszdebinski <58430570+mateuszdebinski@users.noreply.github.com>
2021-02-08 12:17:29 +0100

    Removed incorrect char for postgreSQL (#190)

--- upgrade/db/postgresql/ezplatform-3.1.0-to-3.2.0.sql
+++ upgrade/db/postgresql/ezplatform-3.1.0-to-3.2.0.sql
@@ -1,5 +1,5 @@
 -- EZEE-3244: Added path to site configuration --
-ALTER TABLE `ezsite_public_access` ADD COLUMN site_matcher_path VARCHAR(255) DEFAULT NULL;
+ALTER TABLE ezsite_public_access ADD COLUMN site_matcher_path VARCHAR(255) DEFAULT NULL;
 --
 
 DROP TABLE IF EXISTS ibexa_segment_group_map;
