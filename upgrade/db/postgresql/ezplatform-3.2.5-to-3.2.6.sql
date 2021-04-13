444511584e56ac9cbe04e0d73cd3506682821398
Adam Wójs <adam@wojs.pl>
2021-04-13 15:12:07 +0200

    IBX-153: Workflow queries optimization

--- upgrade/db/postgresql/ezplatform-3.2.5-to-3.2.6.sql
+++ upgrade/db/postgresql/ezplatform-3.2.5-to-3.2.6.sql
@@ -1,4 +1,2 @@
-ALTER TABLE ezpage_attributes MODIFY value LONGTEXT;
-
 CREATE INDEX idx_workflow_co_id_ver ON ezeditorialworkflow_workflows(content_id, version_no);
 CREATE INDEX idx_workflow_name ON ezeditorialworkflow_workflows(workflow_name);
